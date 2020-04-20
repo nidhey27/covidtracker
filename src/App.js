f068a74afdb342bed288136d8f0fc1b8c34c51f3
nidhey27 <nidhey60@gmail.com>
2020-04-20 20:52:16 +0530

    Adding Select country and Charts

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+    
+    this.setState({ data, country: country });
+    console.log(data);
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <h3>-Powered By Nycto Nid</h3>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
